38d892ff3d448524fbd4ec703a62672ecc65f5db
Dhanjeet Kumar Thakur <68640018+DhanjeetKumarThakur@users.noreply.github.com>
2024-05-09 22:26:32 +0530

    Add files via upload

--- Assignment_pooja/src/main/java/com/cg/DAO/UserDao.java
+++ Assignment_pooja/src/main/java/com/cg/DAO/UserDao.java
@@ -3,15 +3,13 @@ package com.cg.DAO;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import com.cg.beans.AdminUser;
 import com.cg.beans.User;
 
 public interface UserDao extends JpaRepository<User, Integer>{
 
 	Optional<User> findByEmail(String email);
 
-	Optional<AdminUser> findById(String username);
+	
 
 }
