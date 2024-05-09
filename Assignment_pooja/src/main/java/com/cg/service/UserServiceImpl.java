b41a4d0978af39e845e43a5f3d93afa09e79ee2c
Dhanjeet Kumar Thakur <68640018+DhanjeetKumarThakur@users.noreply.github.com>
2024-05-09 22:29:37 +0530

    Updated Service package
    Made changes in service package according to our need.

--- Assignment_pooja/src/main/java/com/cg/service/UserServiceImpl.java
+++ Assignment_pooja/src/main/java/com/cg/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.cg.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.cg.DAO.UserDao;
@@ -10,13 +11,17 @@ import com.cg.beans.User;
 
 @Service
 public class UserServiceImpl implements UserService{
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 
 	@Autowired
 	private UserDao userDao;
 	
 	@Override
 	public User saveUser(User user) {
-		user.setRole("ROLE_user");
+		user.setRole("ROLE_NORMAL_USER");
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
 		userDao.save(user);
 		return userDao.save(user);
 	}
